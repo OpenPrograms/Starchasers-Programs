2237da514d135bd7a4f0221eec64565ffeae0a75
KocproZ <kocproz@protonmail.com>
2019-07-28 00:06:32 +0200

    examples

--- oc-sockets/examples/server.example.lua
+++ oc-sockets/examples/server.example.lua
@@ -5,7 +5,9 @@ local component = require('component')
 
 local server = serverLib.constructor(component.modem.address, 1337)
 local socket = server.accept()
+print('Client accepted')
 
 socket.send('U there?')
 print(socket.receive(10))
 socket.close()
+print('closed')
