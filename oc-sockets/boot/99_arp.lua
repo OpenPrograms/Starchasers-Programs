eb06618ebedaae04eb97cce6f66a27c191dcdcd8
Hubert Balcerzak <hubertbalc@gmail.com>
2020-07-02 01:09:40 +0200

    improved dns

--- oc-sockets/boot/99_arp.lua
+++ oc-sockets/boot/99_arp.lua
@@ -1,15 +1,7 @@
 local event = require('event')
 local computer = require('computer')
-local component = require('component')
-local modem = component.modem;
 local dns = require('dns')
-
-if not io.open('/etc/sockets_cfg.lua') == nil then
-    local config = dofile('/etc/sockets_cfg.lua')
-    PORT_ARP = config.arp_port
-else
-    PORT_ARP = 1
-end
+local _packet = require('packet')
 
 
 --
@@ -24,42 +16,23 @@ local sendTimerId
 local expireTimerId
 local hostname
 
-local function receive(_, _, remoteAddress, port, _, msg)
-    if port == 1 then
-        local now = computer.uptime()
-        dns.hostsByName[msg] = { address = remoteAddress, time = now }
-        dns.hostsByAddress[remoteAddress] = { hostname = msg, time = now }
+local function receive(_, _, remoteAddress, port, _, type, msg)
+  if port == dns.PORT_ARP then
+    if type == _packet.type.TYPE_ARP_RESPONSE and msg then
+      local now = computer.uptime()
+      dns.hostsByName[msg] = { address = remoteAddress, time = now }
+      dns.hostsByAddress[remoteAddress] = { hostname = msg, time = now }
+    elseif type == _packet.type.TYPE_ARP_QUESTION and msg and msg == dns.hostname then
+      dns._modem.send(remoteAddress, dns.PORT_ARP, _packet.type.TYPE_ARP_RESPONSE, dns.hostname)
     end
-end
-
-local function send()
-    modem.broadcast(PORT_ARP, hostname)
-end
-
-local function expire()
-    local now = computer.uptime()
-    for k, v in pairs(dns.hostsByName) do
-        if v.time < now - 10 then
-            dns.hostsByName[k] = nil
-            dns.hostsByAddress[v.address] = nil
-        end
-    end
+  end
 end
 
 ----
-local file = io.open("/etc/hostname")
-if file then
-    hostname = file:read("*l")
-    file:close()
-end
 
-if hostname == nil or hostname == '' then
-    print("Hostname not set\n")
-    return
-end
-modem.open(1)
-send()
+
+dns._modem.open(dns.PORT_ARP)
+dns.broadcastHostname()
 event.listen('modem_message', receive)
-sendTimerId = event.timer(10, send, math.huge)
-expireTimerId = event.timer(15, expire, math.huge)
-
+sendTimerId = event.timer(10, dns.broadcastHostname, math.huge)
+expireTimerId = event.timer(15, dns._removeExpired, math.huge)
--- oc-sockets/lib/dns.lua
+++ oc-sockets/lib/dns.lua
@@ -1,14 +1,67 @@
+local component = require('component')
+local packet = require('packet')
+local computer = require('computer')
+
 local dns = {}
 
+local file = io.open("/etc/hostname")
+if file then
+  dns.hostname = file:read("*l")
+  file:close()
+end
+
+if dns.hostname == nil or dns.hostname == '' then
+  print("Hostname not set\n")
+  return
+end
+
+if not io.open('/etc/sockets_cfg.lua') == nil then
+  local config = dofile('/etc/sockets_cfg.lua')
+  dns.PORT_ARP = config.arp_port
+else
+  dns.PORT_ARP = 1
+end
+
+dns._modem = component.modem;
 dns.hostsByName = {} --hostname:string -> {address:string, time:number}
 dns.hostsByAddress = {} --address:string -> {hostname:string, time:number}
 
-dns.getAddress = function(hostname)
-    return dns.hostsByName[hostname].address
+
+
+function dns.getAddress(hostname, shouldAsk)
+  shouldAsk = shouldAsk or true
+  if not dns.hostsByName[hostname] and shouldAsk then
+    dns.askHostname(hostname)
+  end
+  return dns.hostsByName[hostname].address
 end
 
-dns.getHostname = function(address)
-    return dns.hostsByAddress[address].hostname
+function dns.getHostname(address)
+  return dns.hostsByAddress[address].hostname
+end
+
+function dns.broadcastHostname()
+  dns._modem.broadcast(dns.PORT_ARP, packet.type.TYPE_ARP_RESPONSE, dns.hostname)
+end
+
+function dns.askHostname(hostname, timeout)
+  timeout = timeout or 2
+  dns._modem.broadcast(dns.PORT_ARP, packet.type.TYPE_ARP_QUESTION, hostname)
+
+  local startTime = computer.uptime()
+  while not dns.hostsByName[hostname] and computer.uptime() - startTime < timeout do
+    os.sleep(0.1)
+  end
+end
+
+function dns._removeExpired()
+  local now = computer.uptime()
+  for k, v in pairs(dns.hostsByName) do
+    if v.time < now - 10 then
+      dns.hostsByName[k] = nil
+      dns.hostsByAddress[v.address] = nil
+    end
+  end
 end
 
 return dns
